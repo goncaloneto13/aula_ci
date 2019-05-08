d4ac09f9cae52de9f2140ff245fdab2cfa03909c
Guilherme Avelino <gavelino@gmail.com>
2019-05-08 16:40:57 -0300

    commit inicial

---
+++ build/precache-manifest.ea8be9ed2b66404b1ed9e1f45cc3addf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca913d9165900ae5bf5da45f356654e",
+    "url": "/test_travis_ci/index.html"
+  },
+  {
+    "revision": "5c47c2e37a53cc3e50b3",
+    "url": "/test_travis_ci/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/test_travis_ci/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5c47c2e37a53cc3e50b3",
+    "url": "/test_travis_ci/static/js/main.9d0d9840.chunk.js"
+  },
+  {
+    "revision": "de9551c7f08aa7120f72",
+    "url": "/test_travis_ci/static/js/runtime~main.a56f2285.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_travis_ci/static/media/logo.5d5d9eef.svg"
+  }
+]);
